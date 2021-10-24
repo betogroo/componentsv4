2b8f084ad8252426663bf00a94efd74826fd1db9
betogroo <luizhumberto@gmail.com>
2021-10-24 11:18:32 -0300

    refactor: add configurations in vue.config and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
@@ -10,13 +12,18 @@ module.exports = {
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint'
   ],
+
   parserOptions: {
     ecmaVersion: 2020
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/component-definition-name-casing': ['error', 'PascalCase']
   },
+
   overrides: [
     {
       files: [
@@ -27,5 +34,13 @@ module.exports = {
         jest: true
       }
     }
+  ],
+
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
   ]
 }
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  pwa: {
+    name: 'Components v4 (TS)',
+    themeColor: '#000000'
+  }
+}
