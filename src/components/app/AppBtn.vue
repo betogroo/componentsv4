175ef282186babc196cc13a879a7a00d3c8fc443
betogroo <luizhumberto@gmail.com>
2021-10-25 10:38:02 -0300

    feat: add AppBtn component

---
+++ src/components/app/AppBtn.vue
@@ -0,0 +1,26 @@
+<template>
+  <button :type="type">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export default defineComponent({
+  name: 'AppBtn',
+
+  props: {
+    type: {
+      type: String as PropType<string>,
+      required: true
+    },
+    color: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
