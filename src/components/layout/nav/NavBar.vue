befb6208b836f3c5639651b78aaa6185277dfc1a
betogroo <luizhumberto@gmail.com>
2021-11-05 15:35:27 -0300

    feat: useLogout

--- src/components/layout/nav/NavBar.vue
+++ src/components/layout/nav/NavBar.vue
@@ -1,23 +1,52 @@
 <template>
-  <div id="nav">
-    <router-link to="/welcome">Welcome</router-link> |
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="nav">
+    <div class="menu">
+      {{ isPending }}
+      {{ notification }}
+      <router-link to="/">Home</router-link> |
+      <router-link to="/welcome">Welcome</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <div class="profile">
+      <h5>usuario</h5>
+      <button @click.prevent="logout">Sair</button>
+      {{ error }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import useLogout from '@/modules/core/auth/composables/useLogout'
 
 export default defineComponent({
   setup() {
-    return {}
+    const { error, logout: logoutSystem, isPending, notification } = useLogout()
+    const logout = async () => {
+      await logoutSystem()
+      if (!error.value) {
+        console.log('deslogou')
+      }
+    }
+    return {
+      logout,
+      error,
+      isPending,
+      notification
+    }
   }
 })
 </script>
 
 <style lang="scss" scoped>
-#nav {
+.nav,
+.profile {
+  padding: 0;
+  margin: 0;
+  height: 36px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   a {
     font-weight: bold;
     color: #2c3e50;
@@ -26,5 +55,15 @@ export default defineComponent({
       color: #42b983;
     }
   }
+}
+.profile button {
+  margin: 4px;
+  padding: 4px;
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+.profile button:hover {
+  color: red;
 }
 </style>
---
+++ src/modules/core/auth/composables/useLogout.ts
@@ -0,0 +1,40 @@
+import { ref } from 'vue'
+import useAuthErrors from './useAuthErrors'
+import useUtils from '@/composables/useUtils'
+import { getAuth, signOut, UserInfo } from '@/plugins/firebase'
+import { Notification } from '@/types/Notification'
+import { Auth } from '../types/Auth'
+
+const { searchError } = useAuthErrors()
+const { delay, setNotification, resetNotification } = useUtils()
+
+const error = ref<boolean | null>(null)
+const notification = ref<Notification>(resetNotification())
+const isPending = ref(false)
+
+const logout = async (): Promise<void> => {
+  isPending.value = true
+  error.value = null
+  notification.value = resetNotification()
+  const auth = getAuth()
+  try {
+    await signOut(auth)
+    notification.value = setNotification('success', 'logou mano')
+    error.value = false
+  } catch (err: any) {
+    console.log(err.code)
+    error.value = err.code
+    notification.value = setNotification('error', searchError(err.code))
+  }
+}
+
+const useLogout = (): {
+  error: typeof error
+  notification: typeof notification
+  isPending: typeof isPending
+  logout: typeof logout
+} => {
+  return { error, notification, isPending, logout }
+}
+
+export default useLogout
